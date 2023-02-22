4c1b9b732c71e6db290c57fb1cbb2894e370807d
Alex Sv <lexa10489@gmail.com>
2023-02-22 16:21:31 +0300

    2

--- src/main/java/com/example/bankapp/entity/Account.java
+++ src/main/java/com/example/bankapp/entity/Account.java
@@ -1,4 +1,36 @@
 package com.example.bankapp.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.sql.Time;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "bank_account")
 public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    private Long account_number;
+    @NonNull
+    @Size(min = 5,message = "Username must been more than 5 characters" )
+    private String login;
+    @NonNull
+    @Size(min = 5,message = "Password must been more than 5 characters" )
+    private String password;
+    @NonNull
+    private Time cr_time;
+
+    @OneToOne(mappedBy = "account", cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn
+    private User user;
 }
--- src/main/java/com/example/bankapp/entity/Address.java
+++ src/main/java/com/example/bankapp/entity/Address.java
@@ -1,4 +1,31 @@
 package com.example.bankapp.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "bank_address")
 public class Address {
+    @Id
+    private Long address_id;
+
+    @NotNull
+    @Size(min = 5, message = "Country name must been more than 5 characters")
+    private String country;
+    @NotNull
+    @Size(min = 5, message = "Country name must been more than 5 characters")
+    private String city;
+    @NotNull
+    private int house;
+
+    private int apartment;
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "address_id")
+    private User user;
 }
